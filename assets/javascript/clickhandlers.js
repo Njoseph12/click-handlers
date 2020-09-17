fb8db6340c7bb5862fa0446b7ad585d34e0baf85
nathan joseph <josephnathan10@gmail.com>
2020-09-17 13:56:38 -0800

    finished activity

---
+++ assets/javascript/clickhandlers.js
@@ -0,0 +1,27 @@
+const colorBox = document.querySelector("#color-box");
+const grayButton = document.querySelector("#gray-button");
+grayButton.addEventListener("click", function () {
+  colorBox.className = "gray";
+});
+const blueButton = document.querySelector("#blue-button");
+blueButton.addEventListener("click", function () {
+  colorBox.className = "blue";
+});
+const pinkButton = document.querySelector("#pink-button");
+pinkButton.addEventListener("click", function () {
+  colorBox.className = "pink";
+});
+let count = 0;
+const countspan = document.querySelector("#countspan");
+const counterButton = document.querySelector("#counter-button");
+counterButton.addEventListener("click", function () {
+  countspan.innerHTML++;
+});
+const evenOrOddButton = document.querySelector("#even-or-odd-button");
+evenOrOddButton.addEventListener("click", function () {
+  if (countspan.innerHTML % 2 == 0) {
+    alert("This is an Even number");
+  } else {
+    alert("This is an odd number");
+  }
+});
